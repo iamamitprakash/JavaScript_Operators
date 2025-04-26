b54529c5b23f53f30a519ce19a1de7f1cc21c092
Amit Prakash <prakashamit37@gmail.com>
2025-04-26 23:06:44 +0530

    Arithmetic operators

---
+++ arithmeticOperators.js
@@ -0,0 +1,16 @@
+//Write a program to add, subtract, multiply and divide two numbers.
+
+let a = 45;
+let b = 15;
+
+//Add these two numbers
+console.log("The add of these two numbers are:", a + b);
+
+//Subtract these two numbers
+console.log("The subtract of these two numbers are:", a - b);
+
+//Multiply these two numbers
+console.log("The multiply of these two numbers are:", a * b);
+
+//Divide these two numbers
+console.log("The Divide of these two numbers are:", a / b);
